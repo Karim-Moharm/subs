2ba646d938aa160424c12c4ee655735b7bcc6ad9
Karim-Moharm <karimmoharm222@gmail.com>
2024-08-14 15:46:07 +0300

    update the script

--- subs.sh
+++ subs.sh
@@ -23,17 +23,17 @@ mkdir -p "$dir"
 cd "$dir"
 
 echo -e "\033[33m starting subfinder \033[0m"
-subfinder -d $1 -all > subfind.txt
+subfinder -d $1 -all > subfind.txt &
 
 echo "\033[33m starting assetfinder \033[0m"
-assetfinder $1 -subs-only | tee asset.txt
+assetfinder $1 -subs-only | tee asset.txt &
 
 echo "\033[33m starting findomain \033[0m"
-findomain -t $1 -u findomain.txt
+findomain -t $1 -u findomain.txt &
 
 echo "\033[33m starting github-subdomains \033[0m"
 read -p "enter the github token: " gh_token
-github-subdomains -d target.com -t $gh_token -o github-subs.txt
+github-subdomains -d target.com -t $gh_token -o github-subs.txt &
 
 cat subfind.txt asset.txt findomain.txt github-subs.txt > subs.txt
 
@@ -46,8 +46,11 @@ echo "\033[33m number of subdomains after filtering: $filtered_subs \033[0m"
 
 echo "\033[32m getting live subs... \033[0m"
 
-cat subs | httpx | tee httpx.txt
-cat subs | httprobe | tee httprobe.httpx
+cat subs | httpx | tee httpx.txt &
+cat subs | httprobe | tee httprobe.httpx &
+
+
+wait
 
 echo "live domain using httpx: $(cat httpx.txt | wc -l)" 
 echo "live domain using httprobe: $(cat httprobe.txt | wc -l)"
